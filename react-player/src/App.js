e6ffe6462e30ffcea148fc274e75222f0bf1c1ec
nasir <engnasir733@gmail.com>
2020-12-01 17:02:27 +0500

    made the landing layout

---
+++ react-player/src/App.js
@@ -0,0 +1,13 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+import './styles/app.scss'
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-player/src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Song() {
+  return (
+    <div className="song-container">
+      <h1>Song</h1>
+      <h1>Picture</h1>
+      <h1>Name</h1>
+    </div>
+  );
+}
+
+export default Song;
