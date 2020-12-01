d7e49e83d3ae5057935dbc1830aad641e50de0c9
nasir <engnasir733@gmail.com>
2020-12-01 19:57:30 +0500

    made the play and pause button work

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
-import './styles/app.scss'
+import "./styles/app.scss";
+import data from "./util";
+
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
--- react-player/src/components/Player.js
+++ react-player/src/components/Player.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
   faAngleLeft,
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
-function Player() {
+function Player({ currentSong, isPlaying, setIsPlaying }) {
+  const audioRef = useRef(null);
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
   return (
     <div className="player">
       <div className="time-control">
@@ -15,13 +25,19 @@ function Player() {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 }
