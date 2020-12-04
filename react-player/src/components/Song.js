547e8e365b3bf615dee9fdbd896e1c7accc84ac7
nasir <engnasir733@gmail.com>
2020-12-04 17:45:33 +0500

    final commit

--- react-player/src/components/Song.js
+++ react-player/src/components/Song.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-function Song({ currentSong }) {
+const Song = ({ currentSong, isPlaying }) => {
   return (
     <div className="song-container">
-      <img src={currentSong.cover}></img>
+      <img
+        className={isPlaying ? "rotateSong" : ""}
+        src={currentSong.cover}
+        alt=""
+      />
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
   );
-}
+};
 
 export default Song;
